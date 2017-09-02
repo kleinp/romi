32740cf7125366b2cd86bc2d89698c5a6a775fd7
Peter Klein <peter.t.klein@gmail.com>
2017-09-01 21:40:24 -0400

    Moved gazebo model around and added export variables for gazebo in setup.bash

--- catkin_ws/devel/setup.bash
+++ catkin_ws/devel/setup.bash
@@ -4,5 +4,14 @@
 CATKIN_SHELL=bash
 
 # source setup.sh from same directory as this file
-_CATKIN_SETUP_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
-. "$_CATKIN_SETUP_DIR/setup.sh"
+CATKIN_SETUP_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
+. "$CATKIN_SETUP_DIR/setup.sh"
+
+#echo $CATKIN_SETUP_DIR
+
+# export Gazebo paths
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$CATKIN_SETUP_DIR"/../../gazebo_models/romi/plugin/build/"
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$CATKIN_SETUP_DIR"/../../gazebo_models/"
+
+#echo $GAZEBO_PLUGIN_PATH
+#echo $GAZEBO_MODEL_PATH
