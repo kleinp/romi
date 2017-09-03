134a193d0543f84a96136afccba55e71456d2ec6
Peter Klein <peter.t.klein@gmail.com>
2017-09-02 22:44:09 -0400

    Moved gazebo world and robot model files around to match the structure as suggested in the gazebo tutorial: http://gazebosim.org/tutorials?tut=ros_roslaunch&cat=connect_ros

--- catkin_ws/devel/setup.bash
+++ catkin_ws/devel/setup.bash
@@ -10,10 +10,10 @@ CATKIN_SETUP_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd
 #echo $CATKIN_SETUP_DIR
 
 # export Gazebo paths
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$CATKIN_SETUP_DIR"/../../gazebo_models/romi/plugin/build/"
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:"/opt/ros/kinetic/lib/"
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:"/usr/lib/x86_64-linux-gnu/gazebo-8/plugins/"
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$CATKIN_SETUP_DIR"/../../gazebo_models/"
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$CATKIN_SETUP_DIR"/../src/romi_description/plugin/build/"
+#export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:"/opt/ros/kinetic/lib/"
+#export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:"/usr/lib/x86_64-linux-gnu/gazebo-8/plugins/"
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$CATKIN_SETUP_DIR"/../src/"
 
 #echo $GAZEBO_PLUGIN_PATH
 #echo $GAZEBO_MODEL_PATH
